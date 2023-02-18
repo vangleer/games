ab56d7e6b1869dfcb379c7f995ecc12d3316b4b5
huangwantong <huangwantong@escopetech.com>
2023-02-18 16:18:42 +0800

    chore: 幸运大转盘

--- src/components/lottery/Lottery.vue
+++ src/components/lottery/Lottery.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="g-lottery-container">
-    <div class="g-lottery-box">
-      <div v-for="item in lotteryList" :key="item.name" class="g-lottery-item" :style="item">{{ 1 }}</div>
+    <div class="g-lottery">
+      <div
+        class="g-lottery-box1"
+        :style="{ width: state.boxWidth + 'px', height: state.boxWidth + 'px' }"
+      >
+        <div
+          class="g-lottery-box2"
+          :style="{ transform: `rotate(${state.rotate}deg)` }"
+        >
+          <div
+            v-for="item in lotteryList"
+            :key="item.name"
+            class="g-lottery-item"
+            :style="{ width: state.itemWidth + 'px', height: state.itemWidth + 'px', ...item }"
+          >
+            <span>{{ item.name }}</span>
+            <span>{{ item.desc }}</span>
+          </div>
+          <img class="g-lottery-pan g-center" :src="circlePanBlank">
+        </div>
+      </div>
+      <img class="g-lottery-btn g-center" :src="circleBtn" @click="handleStart">
     </div>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { reactive } from 'vue'
+import circlePanBlank from './images/circle_pan_blank.png'
+import circleBtn from './images/circle_btn.png'
+const state = reactive({
+  boxWidth: 400,
+  itemWidth: 80,
+  rotate: 0,
+  circleNum: 0
+})
 const lotteryList: any = reactive([
   { name: 'IPhone14', desc: '手机' },
   { name: '100元', desc: '话费' },
@@ -20,37 +48,82 @@ const lotteryList: any = reactive([
 ])
 
 function transform() {
-  const radius = 200
+  //中心点横坐标
+  var dotLeft = (state.boxWidth - state.itemWidth) / 2;
+  //中心点纵坐标
+  var dotTop = dotLeft;
+
+  const radius = state.boxWidth / 2.5
   const num = lotteryList.length
   const avd = 360 / num
   const ahd = avd * Math.PI / 180
   for (let index = 0; index < num; index++) {
+    const x = Math.cos((ahd*index)) * radius + dotLeft
+    const y = Math.sin((ahd*index)) * radius + dotTop
 
-    const x = Math.cos((ahd*index)) * radius + 185
-    const y = Math.sin((ahd*index)) * radius + 185
-
-    lotteryList[index].transform = `translate(${x}px, ${y}px)`
+    const rotate = avd * (index + 2)
+    lotteryList[index].transform = `translate(${x}px, ${y}px) rotate(${rotate}deg)`
   }
 }
 
+function handleStart() {
+  const len = lotteryList.length
+  const randomIndex = Math.floor(Math.random() * len)
+  state.circleNum += 6
+  const rotate = state.circleNum * 360 - randomIndex * 45
+  state.rotate = rotate
+}
 transform()
 </script>
 
 <style lang='scss' scoped>
-.g-lottery-box {
+.g-lottery {
   position: relative;
   margin: 100px auto;
   width: 400px;
   height: 400px;
-  border: 1px solid red;
-  border-radius: 50%;
-  .g-lottery-item {
-    position: absolute;
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    text-align: center;
-    background-color: greenyellow;
+  .g-lottery-box1 {
+    transform: rotate(-90deg);
+    transform-origin: center;
   }
+  .g-lottery-box2 {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    transform-origin: center;
+    transition: transform 0.6s;
+    .g-lottery-item {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+      text-align: center;
+      z-index: 2;
+      transform-origin: center;
+      color: #EA4661;
+      font-weight: 600;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    .g-lottery-pan {
+      width: 400px;
+      height: 400px;
+    }
+  }
+  .g-lottery-btn {
+    width: 180px;
+    cursor: pointer;
+  }
+}
+
+.g-center {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
